92a49af2fd5abaac503c0b32a97cbc5ee054080b
kalosa0770 <kalosaelijah3@gmail.com>
2024-07-28 06:57:50 +0100

    Updated with functionality

--- css/fontawesome-free-6.5.2-web/js/index.js
+++ css/fontawesome-free-6.5.2-web/js/index.js
@@ -8,4 +8,54 @@ function showSidebar() {
     sidebar.style.display = 'none';
   }
 
+
+
+  
+  let sections = document.querySelectorAll('section');
+
+  window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+
+      if (top >= offset && top < offset + height) {
+        sec.classList.add('show-animate');
+      }
+      else {
+        sec.classList.remove('show-animate');
+      }
+    })
+  }
+
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Changes image every 2 seconds
+  }
+ 
+  
+
+  function zoomin() {
+    document.getElementById("event").style.opacity = 0.7;
+  }
+
+  function zoomout() {
+    document.getElementById("event").style.opacity = 1;
+  }
+
  
